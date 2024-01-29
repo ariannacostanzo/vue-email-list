79e81fd63908fc78ebd10ac5b2bbfa3f0954ea3b
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-29 15:16:25 +0100

    implementing axios get method

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 
 const {createApp} = Vue;
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
 const app = createApp({
     data() {
         return {
+            emails: []
         }
+    },
+    methods: {
+        getEmail() {
+            axios.get(endpoint).then((res) => {
+                const email = res.data.response
+                console.log(email)
+            })
+        }
+    },
+    created() {
+        this.getEmail()
     }
 
 
