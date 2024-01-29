fd426cd87fbcdf9f546257c4d19cddcc932226b4
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-29 22:10:39 +0100

    fixing code to make it simpler

--- js/main.js
+++ js/main.js
@@ -7,16 +7,7 @@ const app = createApp({
     data() {
         return {
             emails: [],
-            isLoading: false,
         }
-    },
-    computed: {
-        checkLoadingState() {
-            if(this.emails.length === 10) {
-                this.isLoading = false
-            }
-            return this.isLoading
-        } 
     },
     methods: {
         getEmail() {
@@ -24,12 +15,9 @@ const app = createApp({
         }
     },
     created() {
-        this.isLoading = true;
         for (let i = 0; i < 10; i++) {this.getEmail()}
-        
     }
-
 })
-
-
 app.mount('#root')
+
+
